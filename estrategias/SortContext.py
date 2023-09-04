be6b3d26759195416c6162e97afb676222289d9a
Pedro Carvalhaes <pedrohwc1998@gmail.com>
2023-09-04 18:34:57 -0400

    change to java

--- estrategias/SortContext.py
+++
@@ -1,7 +0,0 @@
-class SortContext:
-
-    def __init__(self, strategy: function):
-        self.strategy = strategy
-    
-    def execute(self, data: list) -> list:
-        return self.strategy.sort(data)
--- exceptionHandler/argsHandler.py
+++
@@ -1,9 +0,0 @@
-
-class ArgsHandler:
-
-    def __init__(self, args):
-        if (len(args) > 5):
-            print(len(args))
-            raise ValueError("Muitos argumentos fornecidos.\nUso: python script.py inc fim stp rpt")
-        elif (len(args) < 5):
-            raise ValueError("Poucos argumentos fornecidos.\nUso: python script.py inc fim stp rpt")
